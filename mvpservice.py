8dadcfadcbdebd2f0e67c898b412a19247c3c289
Emil <efinesbu@gmail.com>
2019-12-23 20:25:49 -0500

    update

--- mvpservice.py
+++ mvpservice.py
@@ -8,7 +8,7 @@ from python_mysql_dbconfig import read_db_config
 import db_connect_test
 import socket
 import urllib.request
-# from ec2_metadata import ec2_metadata
+from ec2_metadata import ec2_metadata
 import requests
 ###############################################################
 app = Flask(__name__)
@@ -20,7 +20,8 @@ if platform.system() == 'Windows':
 else:
     port_num = 80 # Standard HTTP Port on Cloud
     # publichost = urllib.request.urlopen("http://169.254.169.254/latest/meta-data/public-ipv4").read()
-    publichost = ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4").content
+    # publichost = ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4").content
+    publichost = ec2_metadata.public_ipv4
     print("Public IPV4: ", publichost)
 
     # print("Public IPV4", ec2_metadata.public_ipv4)
